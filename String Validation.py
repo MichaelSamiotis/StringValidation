86143f45eb604470a78bc1964310933272253aab
Michael Samiotis <93983609+MichaelSamiotis@users.noreply.github.com>
2022-10-09 12:24:20 +0300

    Update String Validation.py

--- String Validation.py
+++ String Validation.py
@@ -1,15 +1,17 @@
 #Ask the user to enter a string
-string = input("Please enter a text: ")
+string = input('Please enter a text: ')
 
-#Ask the user to define a key which will be searched in the string
-keyinstring = input("Please enter one or more characters to be found in the text: ")
+#Ask the user to define a key which will be searched inside the string
+keyword = input('Please enter one or more characters to be found in the text: ')
 
 #Search the key in the string
-if keyinstring in string:
+if keyword in string:
+    
 #Return message if key is found 
-    print("The \'{:s}\' has been found in the \'{:s}\' text".format(keyinstring,string))
+    print(f'The keyword {keyword} has been found in the text')
+    
 #Return message if key is NOT found 
 else:
-    print("The \'{:s}\' does not exist in the \'{:s}\' text".format(keyinstring,string))
+    print(f'The keyword {keyword} does not exist in the text')
     
-input("Please press Enter")
+input('Please press Enter to Exit the programme')
